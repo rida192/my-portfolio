c003bcb89612b32adeb6ae3cb9fe656f9ad358dc
unknown <rida19974@gmail.com>
2023-01-09 12:05:25 +0200

    CHORE updating my projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,27 +18,27 @@ export const projects = [
     image: insure,
     github:
       "https://github.com/rida192/Insure-landing-page-using-scss-and-mobile-first-approch",
-    live: "insure-3.netlify.app",
+    live: "https://insure-3.netlify.app",
   },
   {
     id: 3,
     name: "Intro Section with dropdown navigation",
     image: intro,
     github: "https://github.com/rida192/Intro-section-with-dropdown-navigation",
-    live: "intro-section-12.netlify.app",
+    live: "https://intro-section-12.netlify.app",
   },
   {
     id: 4,
     name: "Loopstudios Landing Page",
     image: loop,
     github: "https://github.com/rida192/Loopstudios-landing-page",
-    live: "loop-studio1.netlify.app",
+    live: "https://loop-studio1.netlify.app",
   },
   {
     id: 5,
     name: "Interactive card details",
     image: interactive,
     github: "https://github.com/rida192/Interactive-card-details-form",
-    live: "interactive-card-12.netlify.app",
+    live: "https://interactive-card-12.netlify.app",
   },
 ];
