53afc19d64b1f7cd2b1dcd04411346f7e6715097
unknown <rida19974@gmail.com>
2023-01-09 12:00:49 +0200

    CHORE updating my projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ import moviefy from "../assets/projects/5.png";
 export const projects = [
   {
     id: 1,
-    name: "Interactive card details",
+    name: "Moviefy app for movies",
     image: moviefy,
     github: "https://github.com/rida192/Moviefy",
     live: "https://moviefy-application.vercel.app/",
@@ -18,27 +18,27 @@ export const projects = [
     image: insure,
     github:
       "https://github.com/rida192/Insure-landing-page-using-scss-and-mobile-first-approch",
-    live: "https://rida192.github.io/Insure-landing-page-using-scss-and-mobile-first-approch/",
+    live: "insure-3.netlify.app",
   },
   {
     id: 3,
     name: "Intro Section with dropdown navigation",
     image: intro,
     github: "https://github.com/rida192/Intro-section-with-dropdown-navigation",
-    live: "https://rida192.github.io/Intro-section-with-dropdown-navigation/",
+    live: "intro-section-12.netlify.app",
   },
   {
     id: 4,
     name: "Loopstudios Landing Page",
     image: loop,
     github: "https://github.com/rida192/Loopstudios-landing-page",
-    live: "https://rida192.github.io/Loopstudios-landing-page/",
+    live: "loop-studio1.netlify.app",
   },
   {
     id: 5,
     name: "Interactive card details",
     image: interactive,
     github: "https://github.com/rida192/Interactive-card-details-form",
-    live: "https://rida192.github.io/Interactive-card-details-form/",
+    live: "interactive-card-12.netlify.app",
   },
 ];
